2db864c3fe0c6148bc1d534cb9b81bd47523739c
crystal <noelani.villanueva@gmail.com>
2021-09-04 01:30:51 -0700

    recent changes to the body

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Projects.css";
+
+function Projects() {
+  return <div className="projectParent">PROJECTS</div>;
+}
+
+export default Projects;
---
+++ src/components/TechStack/TechStack.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./TechStack.css";
+
+function TechStack() {
+  return <div className="techstackParent">TECHSTACK</div>;
+}
+
+export default TechStack;
