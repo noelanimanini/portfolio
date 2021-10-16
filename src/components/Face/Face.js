5bd341900b9dfad4d6d9645cae8492a8a24d06d8
crystal <noelani.villanueva@gmail.com>
2021-10-15 21:02:18 -0700

    face additions

--- src/components/Face/Face.js
+++ src/components/Face/Face.js
@@ -6,29 +6,14 @@ import face from "../images/face2.jpg";
 function Face() {
   return (
     <div className="faceParent">
-      {/* <h1 className="cyberpunk glitched">Crystal</h1> */}
-      <div class="hexa">
-        <div class="hex1">
-          <div class="hex2">
-            <img src={face} alt="face" className="face" />
-          </div>
-        </div>
+      <div class="facebox">
+        <img src={face} alt="face" className="face" />
       </div>
       <div className="contactInfo">
-        <div className="contactLine">
-          <div>email</div>
-          <div>linkedin</div>
-          <div>medium</div>
-          <div>resume</div>
-        </div>
-        {/* <div className="contactLine">
-          blog:
-          <div></div>
-        </div>
-        <div className="contactLine">
-          linkedin:
-          <div></div>
-        </div> */}
+        <div>email</div>
+        <div>linkedin</div>
+        <div>medium</div>
+        <div>resume</div>
       </div>
     </div>
   );
